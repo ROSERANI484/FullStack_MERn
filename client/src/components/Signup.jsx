4a948ddaae973689a061b63020f17fd8e0388f29
ROSERANI484 <156175623+ROSERANI484@users.noreply.github.com>
2025-11-27 16:58:10 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = () => {
     const { name, email, phone, work, password, cpassword } = user;
 
     try {
-      const res = await fetch("http://localhost:5000/register", {
+      const res = await fetch("https://fullstack-mern-lebd.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
